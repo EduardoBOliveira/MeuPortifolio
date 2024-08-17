d8bd35c6be9a13422fc0511409a775b782d316b2
Eduardo <rm98890@fiap.com.br>
2024-08-17 13:29:19 -0300

    Ajuste na Tela INICIO

--- Portifolio/src/Pages/Home/index.jsx
+++ Portifolio/src/Pages/Home/index.jsx
@@ -8,109 +8,138 @@ import "./sobre.css";
 import "./tecnologias.css";
 
 
+
 export default function Home() {
   return (
     <>
-      <Header />
       <main className="container">
         <article className="intro">
-          <div className="intro_tipografias">
-            <p className="intro_tipografias_textos_introducao">Olá!</p>
-
-            <p className="intro_tipografias_textos_introducao">
-              Seja Bem Vindo!
-            </p>
-
-            <p className="intro_tipografias_textos_apresentacao">
-              Me chamo <span className="destaque">Eduardo Bezerra</span>, um jovem 
-              desenvolvedor, com grande anseio de trabalhar com grandes projetos relacionados a Tecnologia.
-            </p>
-
-            <p className="intro_tipografias_textos_apresentacao">
-              <span className="destaque"> Vamos conversar?</span>
-            </p>
-
-            <div className="intro_tipografias_botoes">
-              <ul className="intro_tipografias_botoes_lista">
-                <li className="intro_tipografias_botoes_lista_item">
-                  <button className="botao">
-                    <a
-                      href="https://www.linkedin.com/in/eduardo-bezerra-oliveira/"
-                      target="_blank"
-                      className="link"
-                    >
-                      LinkedIn
-                    </a>
-                  </button>
-
+          <div className="corpo_da_tela_intro">
+           
+            <header className="container_header" id="inicio">
+              <ul className="header_lista">
+                <li className="header_lista_item">
+                  <a href="#inicio" className="header_lista_item_link">
+                    INICIO
+                  </a>
                 </li>
-                <li className="intro_tipografias_botoes_lista_item">
-                  <button className="botao">
-                    <a href="#contato" className="link">
-                      E-mail
-                    </a>
-                  </button>
+                <li className="header_lista_item">
+                  <a href="#sobre" className="header_lista_item_link">
+                    SOBRE
+                  </a>
                 </li>
-
-                <li className="intro_tipografias_botoes_lista_item">
-                  <button className="botao">
-                    <a
-                      href="https://github.com/EduardoBOliveira"
-                      target="_blank"
-                      className="link"
-                    >
-                      GitHub
-                    </a>
-                  </button>
+                <li className="header_lista_item">
+                  <a href="#tecnologias" className="header_lista_item_link">
+                    SKILLS
+                  </a>
+                </li>
+                <li className="header_lista_item">
+                  <a href="#projetos" className="header_lista_item_link">
+                    PROJETOS
+                  </a>
+                </li>
+                <li className="header_lista_item">
+                  <a href="#contato" className="header_lista_item_link_contato">
+                    CONTATO
+                  </a>
                 </li>
               </ul>
+            </header>
+
+            <div>
+              <div className="intro_tipografias">
+                <p className="intro_tipografias_textos_introducao">Olá!</p>
+
+                <p className="intro_tipografias_textos_introducao">
+                  Seja Bem Vindo!
+                </p>
+
+                <p className="intro_tipografias_textos_apresentacao">
+                  Me chamo <span className="destaque">Eduardo Bezerra</span>, um jovem 
+                  desenvolvedor, com grande anseio de trabalhar com grandes projetos relacionados a Tecnologia.
+                </p>
+
+                <p className="intro_tipografias_textos_apresentacao">
+                  <span className="destaque"> Vamos conversar?</span>
+                </p>
+
+                <div className="intro_tipografias_botoes">
+                  <ul className="intro_tipografias_botoes_lista">
+                    <li className="intro_tipografias_botoes_lista_item">
+                      <button className="botao">
+                        <a
+                          href="https://www.linkedin.com/in/eduardo-bezerra-oliveira/"
+                          target="_blank"
+                          className="link"
+                        >
+                          LinkedIn
+                        </a>
+                      </button>
+
+                    </li>
+                    <li className="intro_tipografias_botoes_lista_item">
+                      <button className="botao">
+                        <a href="#contato" className="link">
+                          E-mail
+                        </a>
+                      </button>
+                    </li>
+
+                    <li className="intro_tipografias_botoes_lista_item">
+                      <button className="botao">
+                        <a
+                          href="https://github.com/EduardoBOliveira"
+                          target="_blank"
+                          className="link"
+                        >
+                          GitHub
+                        </a>
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
             </div>
           </div>
-
         </article>
 
         <article className="sobre">
-          <div className="sobre_titulo" id="sobre">
-            <p className="titulo_atras">SOBRE</p>
-            <p className="titulo_frente">Quem sou eu?</p>
-          </div>
-          <div className="sobre_container">
-            <img
-              src={FotoSecundaria}
-              alt="Foto do desenvolvedor Eduardo Bezerra, um homem branco com cabelo castanho claro e olhos castanhos."
-              className="sobre_foto"
-            />
-            <div className="container_texto">
-              <p className="sobre_texto">
-                Sou Eduardo Bezerra, um desenvolvedor apaixonado por tecnologia e inovação.
-                Com experiência sólida em programação e análise de dados,
-                meu foco está em transformar dados em insights acionáveis e soluções eficientes.
-                Possuo uma formação robusta em programação e uma experiência prática significativa como
-                Analista de Dados, onde construí dashboards interativos e relatórios detalhados
-                utilizando Power BI e SQL Server.
-              </p>
-              <p className="sobre_texto">
-                <span className="destaque">Habilidades em Programação:</span> Proficiente em linguagens de programação,
-                com uma forte capacidade para resolver problemas complexos e otimizar processos.
-              </p>
-              <p className="sobre_texto">
-                <span className="destaque">Análise de Dados:</span> Experiência prática em análise de dados,
-                extração e manipulação de grandes volumes de dados para gerar insights valiosos.
-                Utilizo ferramentas como SQL Server para consultas avançadas e
-                Power BI para criar visualizações interativas e intuitivas.
-              </p>
-              <p className="sobre_texto">
-                <span className="destaque">Criação de Dashboards:</span> Habilidade em desenvolver dashboards personalizados no Power BI,
-                facilitando a visualização e interpretação de dados complexos para suportar a tomada de decisões
-                estratégicas.
-              </p>
-            </div>
+          <div id="sobre">
+            <Header />
+              <div className="sobre_titulo">
+                <p className="titulo_atras">SOBRE</p>
+                <p className="titulo_frente">Quem sou eu?</p>
+              </div>
+
+              <div className="sobre_container">
+                <img
+                  src={FotoSecundaria}
+                  alt="Foto do desenvolvedor Eduardo Bezerra, um homem branco com cabelo castanho claro e olhos castanhos."
+                  className="sobre_foto"
+                />
+                <div className="container_texto">
+                  <p className="sobre_texto">
+                    
+                  </p>
+                  <p className="sobre_texto">
+                    
+                  </p>
+                  <p className="sobre_texto">
+                    
+                  </p>
+                
+                  <p className="sobre_texto">
+                    
+                  </p>
+                </div>
+              </div>
           </div>
         </article>
 
         <article className="tecnologias">
           <p className="tecnologias_titulo" id="tecnologias">
-            Tecnologias
+            SKILLS
           </p>
           <p className="tecnologias_texto">
             Minha linguagem dominante é <span className="destaque">Python</span>, porem possuo conhecimento em Java, Spring, e frameworks Web,
