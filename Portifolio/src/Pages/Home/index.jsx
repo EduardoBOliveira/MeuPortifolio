5aeeccebe2ca0abe732601b904d0e1b55fa1b415
rm98890 <rm98890@fiap.com.br>
2024-08-21 20:31:13 -0300

    Atualizando pagina SOBRE

--- Portifolio/src/Pages/Home/index.jsx
+++ Portifolio/src/Pages/Home/index.jsx
@@ -100,104 +100,112 @@ export default function Home() {
                   className="sobre_foto"
                 />
                 <div className="container_texto">
-                  <p className="sobre_texto">
-                    
-                  </p>
-                  <p className="sobre_texto">
-                    
-                  </p>
-                  <p className="sobre_texto">
-                    
-                  </p>
-                
-                  <p className="sobre_texto">
-                    
-                  </p>
+                  <div className="texto01">
+                    <p className="sobre_texto">
+                      Sou um programador com foco em Python, 
+                      apaixonado por transformar desafios em soluções criativas e eficientes. 
+                    </p>
+                  </div>
+                  <div className="texto02"> 
+                    <p className="sobre_texto">
+                      Minha jornada profissional inclui um estágio como Analista de Dados, 
+                      onde desenvolvi habilidades valiosas no uso de Power BI e SQL Server para análise e visualização de dados.
+                    </p>
+                  </div>
+                  <div className="texto03">
+                    <p className="sobre_texto">
+                      Essa experiência me proporcionou uma visão ampla de como integrar e otimizar dados para gerar insights estratégicos, 
+                      fortalecendo minha capacidade de aplicar conhecimento técnico em contextos práticos e dinâmicos.
+                    </p>
+                  </div>
                 </div>
               </div>
           </div>
         </article>
 
         <article className="tecnologias">
-          <p className="tecnologias_titulo" id="tecnologias">
-            SKILLS
-          </p>
-          <p className="tecnologias_texto">
-            Minha linguagem dominante é <span className="destaque">Python</span>, porem possuo conhecimento em Java, Spring, e frameworks Web,
-            além de ter estagiado em Analise de Dados, utilizando ferramentas como Power BI e SQL Server .
-          </p>
+          <div className="corpo_tenologia" id="tecnologias">
+            
+            <p className="tecnologias_titulo">
+              SKILLS
+            </p>
+            <p className="tecnologias_texto">
+              Minha linguagem dominante é <span className="destaque">Python</span>, porem possuo conhecimento em Java, Spring, e frameworks Web,
+              além de ter estagiado em Analise de Dados, utilizando ferramentas como Power BI e SQL Server .
+            </p>
 
-          <div className="tecnologias_icones">
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
-              alt="html5"
-              height="80px"
-              width="80px"
-            />
+            <div className="tecnologias_icones">
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+                alt="html5"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
-              alt="css3"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+                alt="css3"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
-              alt="git"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+                alt="git"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
-              alt="java"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
+                alt="java"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg"
-              alt="spring"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg"
+                alt="spring"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-              alt="javascript"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+                alt="javascript"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
-              alt="typescript"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+                alt="typescript"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-              alt="react"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+                alt="react"
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg"
-              alt="sqlserver  "
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg"
+                alt="sqlserver  "
+                height="80px"
+                width="80px"
+              />
 
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
-              alt="python"
-              height="80px"
-              width="80px"
-            />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+                alt="python"
+                height="80px"
+                width="80px"
+              />
 
+            </div>
           </div>
         </article>
         <article className="projetos">
